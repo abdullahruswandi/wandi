b85762c46b45eb553b77aca4765155dfea23424e
abdullahruswandi <abdullahruswandi016@gmail.com>
2020-09-30 15:57:29 +0700

    project pertama

---
+++ resources/views/home.blade.php
@@ -0,0 +1,293 @@
+<!DOCTYPE html>
+<!--
+Template Name: Bistup
+Author: <a href="https://www.os-templates.com/">OS Templates</a>
+Author URI: https://www.os-templates.com/
+Copyright: OS-Templates.com
+Licence: Free to use under our free template licence terms
+Licence URI: https://www.os-templates.com/template-terms
+-->
+<html lang="">
+<!-- To declare your language - read more here: https://www.w3.org/International/questions/qa-html-language-declarations -->
+<head>
+<title>Bistup</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="assets/home/layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body id="top">
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row0">
+  <div id="topbar" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <div class="fl_left">
+      <ul class="nospace">
+        <li><a href="index.html"><i class="fas fa-home fa-lg"></i></a></li>
+        <li><a href="/wandi/public/index">Admin</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="/wandi/public/login">Login</a></li>
+        <li><a href="#">Register</a></li>
+      </ul>
+    </div>
+  
+    <!-- ################################################################################################ -->
+  </div>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper bgded overlay" style="background-image:url('assets/home/images/demo/backgrounds/g.jpeg');">
+  <div id="pageintro" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <article>
+      <p></p>
+      <h3 class="heading">menyediakan layanan test covid-19</h3>
+      <p>COV.id_19</p>
+      
+    </article>
+    <!-- ################################################################################################ -->
+  </div>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row3">
+  <main class="hoc container clear"> 
+    <!-- main body -->
+    <!-- ################################################################################################ -->
+    
+    <!-- ################################################################################################ -->
+    <hr class="btmspace-80">
+    <!-- ################################################################################################ -->
+    <section>
+      <div class="sectiontitle">
+        <h6 class="heading">tempat yang menydiakan test covid-19</h6>
+        <p></p>
+      </div>
+      <ul class="nospace group overview">
+        <li class="one_third">
+          <figure><a href="#"><img src="assets/home/images/demo/mitra.jpg" alt=""></a>
+            <figcaption>
+              <h6 class="heading">rumah sakit mitra</h6>
+              <p>Jl. Satelit Indah II, Darmo Satelit, Tanjungsari, Suko Manunggal, Kota SBY, Jawa Timur 60187, Indonesia</p>
+            </figcaption>
+          </figure>
+        </li>
+        <li class="one_third">
+          <figure><a href="#"><img src="assets/home/images/demo/parahita.jpg" alt=""></a>
+            <figcaption>
+              <h6 class="heading">rumah sakit parahita</h6>
+              <p>Jl. Raya Mulyosari No.105-105A, Kalisari, Kec. Mulyorejo, Kota SBY, Jawa Timur 60112.</p>
+            </figcaption>
+          </figure>
+        </li>
+        <li class="one_third">
+          <figure><a href="#"><img src="assets/home/images/demo/gloskin.jpg" alt=""></a>
+            <figcaption>
+              <h6 class="heading">klinik gloskin</h6>
+              <p>Ruko Grand Sungkono, Jl. Mayjen Sungkono No.176-178, Dukuh Pakis, Kec. Dukuhpakis, Kota SBY, Jawa Timur 60225</p>
+            </figcaption>
+          </figure>
+        </li>
+        <li class="one_third">
+          <figure><a href="#"><img src="assets/home/images/demo/biotest.jpg" alt=""></a>
+            <figcaption>
+              <h6 class="heading">biotest</h6>
+              <p>Jl. Raya Kertajaya Indah Blok F304 No. 35, Manyar Sabrangan, Kec. Mulyorejo, Kota SBY, Jawa Timur 60116</p>
+            </figcaption>
+          </figure>
+        </li>
+        <li class="one_third">
+          <figure><a href="#"><img src="assets/home/images/demo/4.jpg" alt=""></a>
+            <figcaption>
+              <h6 class="heading">Cahaya Diagnostic Centre</h6>
+              <p>Jalan Dharmahusada Utara 36-38, blok G-J, Mojo, Kec. Gubeng, Kota SBY, Jawa Timur.</p>
+            </figcaption>
+          </figure>
+        </li>
+        <li class="one_third">
+          <figure><a href="#"><img src="assets/home/images/demo/prodia.jpg" alt=""></a>
+            <figcaption>
+              <h6 class="heading">Laboratorium Klinik Prodia Jemursari</h6>
+              <p>39, Jl. Raya Jemursari, Jemur Wonosari, Kec. Wonocolo, Kota SBY, Jawa Timur</p>
+            </figcaption>
+          </figure>
+        </li>
+      </ul>
+    </section>
+    <!-- ################################################################################################ -->
+    <!-- / main body -->
+    <div class="clear"></div>
+  </main>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper coloured">
+  <article class="hoc cta clear"> 
+    <!-- ################################################################################################ -->
+    <h6 class="three_quarter first">Dis parturient montes nascetur ridiculus mus vivamus</h6>
+    <footer class="one_quarter"><a class="btn" href="#">Porta risus purus</a></footer>
+    <!-- ################################################################################################ -->
+  </article>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row3">
+  <section class="hoc container clear"> 
+    <!-- ################################################################################################ -->
+    <div class="sectiontitle">
+      <h6 class="heading">Et ipsum suscipit sit amet</h6>
+      <p>Molestie dolor blandit mauris porta quam erat ut laoreet velit</p>
+    </div>
+    <ul class="nospace group latest">
+      <li class="one_third first">
+        <article>
+          <time datetime="2045-04-06T08:15+00:00"><i class="far fa-calendar-alt rgtspace-5"></i> 06<sup>th</sup> April 2045 @ 15:00pm</time>
+          <div class="excerpt">
+            <h6 class="heading">ac finibus purus pulvinar quis suspendisse varius</h6>
+            <p>condimentum sem ac euismod tortor semper sit amet fusce pretium justo nec faucibus finibus libero odio porttitor magna et semper mi velit maximus nisi nullam accumsan massa [<a href="#">&hellip;</a>]</p>
+            <ul class="meta">
+              <li><i class="fas fa-user rgtspace-5"></i> <a href="#">Admin</a></li>
+              <li><i class="fas fa-tags rgtspace-5"></i> <a href="#">Tag 1</a>, <a href="#">Tag 2</a></li>
+            </ul>
+            <footer><a class="btn" href="#">Read More</a></footer>
+          </div>
+        </article>
+      </li>
+      <li class="one_third">
+        <article>
+          <time datetime="2045-04-05T08:14+00:00"><i class="far fa-calendar-alt rgtspace-5"></i> 05<sup>th</sup> April 2045 @ 14:00pm</time>
+          <div class="excerpt">
+            <h6 class="heading">dapibus nulla a tristique risus vivamus ut metus</h6>
+            <p>turpis integer ac neque ac risus malesuada tincidunt vestibulum rutrum luctus lobortis ut tincidunt nulla at luctus aliquet mi magna sagittis sem id dapibus dui ex at augue etiam [<a href="#">&hellip;</a>]</p>
+            <ul class="meta">
+              <li><i class="fas fa-user rgtspace-5"></i> <a href="#">Admin</a></li>
+              <li><i class="fas fa-tags rgtspace-5"></i> <a href="#">Tag 1</a>, <a href="#">Tag 2</a></li>
+            </ul>
+            <footer><a class="btn" href="#">Read More</a></footer>
+          </div>
+        </article>
+      </li>
+      <li class="one_third">
+        <article>
+          <time datetime="2045-04-04T08:13+00:00"><i class="far fa-calendar-alt rgtspace-5"></i> 04<sup>th</sup> April 2045 @ 13:00pm</time>
+          <div class="excerpt">
+            <h6 class="heading">lacinia et mauris sit amet bibendum vulputate sem</h6>
+            <p>duis vulputate at massa et sollicitudin erat eget suspendisse dignissim ultrices hendrerit donec aliquam risus a porta fermentum donec a mi pulvinar ex auctor eleifend eu finibus [<a href="#">&hellip;</a>]</p>
+            <ul class="meta">
+              <li><i class="fas fa-user rgtspace-5"></i> <a href="#">Admin</a></li>
+              <li><i class="fas fa-tags rgtspace-5"></i> <a href="#">Tag 1</a>, <a href="#">Tag 2</a></li>
+            </ul>
+            <footer><a class="btn" href="#">Read More</a></footer>
+          </div>
+        </article>
+      </li>
+    </ul>
+    <!-- ################################################################################################ -->
+  </section>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row2">
+  <figure class="hoc container clear clients"> 
+    <!-- ################################################################################################ -->
+    <ul class="nospace group">
+      <li class="one_quarter first"><a href="#"><img src="assets/home/images/demo/249x100.png" alt=""></a></li>
+      <li class="one_quarter"><a href="#"><img src="assets/home/images/demo/249x100.png" alt=""></a></li>
+      <li class="one_quarter"><a href="#"><img src="assets/home/images/demo/249x100.png" alt=""></a></li>
+      <li class="one_quarter"><a href="#"><img src="assets/home/images/demo/249x100.png" alt=""></a></li>
+    </ul>
+    <!-- ################################################################################################ -->
+  </figure>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row4">
+  <footer id="footer" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <div class="one_quarter first">
+      <h6 class="heading">Nulla ullamcorper</h6>
+      <ul class="nospace btmspace-30 linklist contact">
+        <li><i class="fas fa-map-marker-alt"></i>
+          <address>
+          Street Name &amp; Number, Town, Postcode/Zip
+          </address>
+        </li>
+        <li><i class="fas fa-phone"></i> +00 (123) 456 7890</li>
+        <li><i class="far fa-envelope"></i> info@domain.com</li>
+      </ul>
+      <ul class="faico clear">
+        <li><a class="faicon-facebook" href="#"><i class="fab fa-facebook"></i></a></li>
+        <li><a class="faicon-twitter" href="#"><i class="fab fa-twitter"></i></a></li>
+        <li><a class="faicon-dribble" href="#"><i class="fab fa-dribbble"></i></a></li>
+        <li><a class="faicon-linkedin" href="#"><i class="fab fa-linkedin"></i></a></li>
+      </ul>
+    </div>
+    <div class="one_quarter">
+      <h6 class="heading">Dignissim nibh ut</h6>
+      <ul class="nospace linklist">
+        <li><a href="#">Natoque penatibus et magnis</a></li>
+        <li><a href="#">Dis parturient montes</a></li>
+        <li><a href="#">Nascetur ridiculus mus</a></li>
+        <li><a href="#">Vestibulum tincidunt nisi</a></li>
+        <li><a href="#">Sed eleifend scelerisque</a></li>
+      </ul>
+    </div>
+    <div class="one_quarter">
+      <h6 class="heading">Vestibulum cras placerat</h6>
+      <ul class="nospace linklist">
+        <li><a href="#">Maecenas vestibulum molestie</a></li>
+        <li><a href="#">Arcu cras sed tincidunt</a></li>
+        <li><a href="#">Enim maecenas sed mi dictum</a></li>
+        <li><a href="#">Dolor laoreet fringilla</a></li>
+        <li><a href="#">Augue curabitur lobortis</a></li>
+      </ul>
+    </div>
+    <div class="one_quarter">
+      <h6 class="heading">Vulputate cum sociis</h6>
+      <p class="nospace btmspace-15">Diam nulla id condimentum magna scelerisque ac suspendisse viverra est purus.</p>
+      <form method="post" action="#">
+        <fieldset>
+          <legend>Newsletter:</legend>
+          <input class="btmspace-15" type="text" value="" placeholder="Name">
+          <input class="btmspace-15" type="text" value="" placeholder="Email">
+          <button type="submit" value="submit">Submit</button>
+        </fieldset>
+      </form>
+    </div>
+    <!-- ################################################################################################ -->
+  </footer>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row5">
+  <div id="copyright" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <p class="fl_left">Copyright &copy; 2018 - All Rights Reserved - <a href="#">Domain Name</a></p>
+    <p class="fl_right">Template by <a target="_blank" href="https://www.os-templates.com/" title="Free Website Templates">OS Templates</a></p>
+    <!-- ################################################################################################ -->
+  </div>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<a id="backtotop" href="#top"><i class="fas fa-chevron-up"></i></a>
+<!-- JAVASCRIPTS -->
+<script src="assets/home/layout/scripts/jquery.min.js"></script>
+<script src="assets/home/layout/scripts/jquery.backtotop.js"></script>
+<script src="assets/home/layout/scripts/jquery.mobilemenu.js"></script>
+</body>
+</html> 
